56a4e2ee7238f0bcfc8ff96ba96592d54c939fce
Daniel Fosco <danielfosco@gmail.com>
2022-06-28 21:23:31 +0200

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,19 @@
 module.exports = config => {
   // Input directory: src
   // Output directory: public
-
+  
   // The following copies to `public/assets`
   config.addPassthroughCopy("src/assets");
+
+  // Copy CSS files
+  config.addPassthroughCopy('src/style/style.css')
 
   // Find and copy any `jpg` files, maintaining directory structure.
   // eleventyConfig.addPassthroughCopy("**/*.jpg");
 
   // 11ty defaults
   return {
+    passthroughFileCopy: true,
     dir: {
       input: 'src',
       output: '_site',
@@ -18,3 +22,10 @@ module.exports = config => {
   };
 };
 
+
+// module.exports = function(eleventyConfig) {
+//   eleventyConfig.addPassthroughCopy('css')
+//   return {
+//     passthroughFileCopy: true
+//   }
+// }
